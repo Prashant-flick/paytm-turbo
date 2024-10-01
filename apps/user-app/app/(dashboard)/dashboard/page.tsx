b3a583a763a54a9258f3504731138dfc8a234f10
Prashant Kumar <prashantkumar1251@gmail.com>
2024-10-01 07:28:17 +0530

    changes in dashboard home page

--- apps/user-app/app/(dashboard)/dashboard/page.tsx
+++ apps/user-app/app/(dashboard)/dashboard/page.tsx
@@ -1,6 +1,7 @@
 
 export default function() {
-    return <div>
-        Dashboard Page
+    return <div className="p-10">
+        <h1 className="text-3xl text-purple-700 font-bold mb-6">DASHBOARD</h1>
+        <h1 className="text-2xl text-black font-bold">WELCOME TO PAYTM</h1>
     </div>
 }
